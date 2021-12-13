827dc257eb1c2adeafa82ecb131d696ac2dddb1b
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-12-13 09:24:56 -0500

    Add files via upload

--- darrylmcoder-proxy/main.php
+++ darrylmcoder-proxy/main.php
@@ -7,3 +7,9 @@ ini_set('display_errors', 1);
 echo "REQUEST_URI: {$_SERVER['REQUEST_URI']}<br>\n";
 
 echo "PATH: {$_GET['url']}<br>\n";
+
+$splits = explode("darrylmcoder-proxy/",$_SERVER['REQUEST_URI']);
+
+$url = $splits[1];
+
+echo "SPLIT: {$url}";
